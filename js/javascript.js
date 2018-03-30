8e97c6d3cfdc7fd343b9cb8c7dea04330e42a547
Michal Goluch <goluch.michal89@gmail.com>
2018-03-31 00:27:33 +0200

    add command lines in javascript for task purpose

--- js/javascript.js
+++ js/javascript.js
@@ -12,3 +12,20 @@ var h = prompt("Enter height value");
 var triangleArea = 0.5 * a * h;
 alert("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
 console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+
+var a = prompt("Enter first value");
+var b = prompt("Enter second value");
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log("The result is: " + value);
+
+if (value>0) {
+    console.log("Value " + value + " is a positive result");
+} else {
+    console.log("Value " + value + " is a negative result");
+}
+
+if (value == 0) {
+    console.log("Value is: " + value);
+} else {
+    console.log("Value isn't zero");
+}
